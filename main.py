7afb0e5b3a69337bd4d3a09e6538df8d46cae563
NevisZace <nevis.zace@city.ac.uk>
2025-12-08 14:26:39 +0000

    data description

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 
 plt.rcParams["figure.figsize"] = (10, 5)
 BankData = pd.read_csv("bank.csv")
+
+#shows the data types of each column  and statistics related to it
+print("Shape:", BankData.shape)
+print(BankData.dtypes)
+pd.set_option('display.max_columns', None)
+print(BankData.describe(include="all"))
+
+#no missing values
+#print(BankData.isnull().sum())
