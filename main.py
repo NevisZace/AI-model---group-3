8327a8b41be2d124e7413778e8c069015c185448
shadzj <Shadi.Jahangiri@city.ac.uk>
2025-12-15 18:51:47 +0000

    Initial EDA

--- main.py
+++ main.py
@@ -12,4 +12,21 @@ pd.set_option('display.max_columns', None)
 print(BankData.describe(include="all"))
 
 #no missing values
-#print(BankData.isnull().sum())
+print(BankData.isnull().sum())
+
+
+# target distribution
+print(BankData['deposit'].value_counts())
+print(BankData['deposit'].value_counts(normalize=True))
+BankData['deposit'].value_counts().plot(kind='bar')
+plt.title("Target Distribution: Deposit")
+plt.xlabel("Deposit")
+plt.ylabel("Count")
+plt.show()
+
+#define data and target
+#X = BankData.drop(columns=["deposit"])
+#y = BankData["deposit"]
+
+#print("X shape:", X.shape)
+#print("y shape:", y.shape)
