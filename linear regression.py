48fbf21b37c4d1ad0a4563c3c688a465196683a6
NevisZace <nevis.zace@city.ac.uk>
2025-12-17 17:01:19 +0000

    linear regression model added

--- linear regression.py
+++ linear regression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 
 BankData = pd.read_csv("bank.csv")
@@ -13,6 +13,7 @@ category = X.select_dtypes(include='object').columns
 label = LabelEncoder()
 for col in category:
     X[col] = label.fit_transform(X[col])
+y = y.map({'no': 0, 'yes': 1})
 
 #Train/test split
 X_train, X_test, y_train, y_test = train_test_split(
@@ -21,19 +22,19 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42,
     stratify=y
 )
-
-dt_model = DecisionTreeClassifier(random_state=42)
-dt_model.fit(X_train, y_train)
-y_pred = dt_model.predict(X_test)
+lrModel = LinearRegression()
+lrModel.fit(X_train, y_train)
+y_pred_continuous = lrModel.predict(X_test)
+y_pred = (y_pred_continuous >= 0.5).astype(int)
 
 print("Performance")
 print(f"Accuracy:  {accuracy_score(y_test, y_pred):.4f}")
-print(f"Precision: {precision_score(y_test, y_pred, pos_label='yes'):.4f}")
-print(f"Recall:    {recall_score(y_test, y_pred, pos_label='yes'):.4f}")
-print(f"F1-score:  {f1_score(y_test, y_pred, pos_label='yes'):.4f}")
+print(f"Precision: {precision_score(y_test, y_pred):.4f}")
+print(f"Recall:    {recall_score(y_test, y_pred):.4f}")
+print(f"F1-score:  {f1_score(y_test, y_pred):.4f}")
 
 print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 
 print("\nClassification Report:")
-print(classification_report(y_test, y_pred))
+print(classification_report(y_test, y_pred, target_names=['no', 'yes']))
